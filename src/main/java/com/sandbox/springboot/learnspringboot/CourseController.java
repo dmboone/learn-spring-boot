bf1582bfd81e190f458255d853570f664c029b2c
Destiny Boone <destaj.db@gmail.com>
2023-02-16 15:47:39 -0500

    build a hello world api with spring boot

---
+++ src/main/java/com/sandbox/springboot/learnspringboot/CourseController.java
@@ -0,0 +1,21 @@
+package com.sandbox.springboot.learnspringboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// simple restAPI
+
+@RestController
+public class CourseController {
+	@RequestMapping("/courses") // map this data to url/courses
+	public List<Course> retrieveAllCourses(){
+		return Arrays.asList(
+			new Course(1, "Learn AWS", "in28minutes"),
+			new Course(2, "Learn DevOps", "in28minutes")
+		);
+	}
+
+}
